bf1c205d852fb33e1fb9b5da372fa76f3d098047
Cesinha <rm99595@estudante.fieb.edu.br>
2026-06-17 14:58:00 +0000

    Croação do projeto spring boot

---
+++ apikitenix/src/main/java/com/itb/inf2fm/apikitenix/ApikitenixApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.apikitenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikitenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApikitenixApplication.class, args);
+	}
+
+}
---
+++ apikitenix/src/test/java/com/itb/inf2fm/apikitenix/ApikitenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.apikitenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApikitenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
